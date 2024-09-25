1b834f96ee32419cb6707b568bd3929f8ab7340c
hgiulliano <henrique.giulliano@gmail.com>
2024-09-25 02:49:25 -0300

    ex36 update and ex37 solved

---
+++ _Mundo2/Aula12/ex37.py
@@ -0,0 +1,24 @@
+while True:
+    number = input('Digite um valor inteiro (Digite exit para sair) :')
+    if number == 'exit':
+        break
+    try:
+        number = int(number)
+    except ValueError:
+        print('Digite um valor inteiro válido.')
+        continue # retorna ao inicio do loop
+    while True:
+        print('[1] Converter para BINÁRIO: ')
+        print('[2] Converter para OCTAL: ')
+        print('[3] Converter para HEXADECIMAL: ')
+        option = int(input('Sua opção: '))
+        if option not in [1,2,3]: # SE A OPÇÃO NÃO ESTÁ EM UM DESSES VALORES DA LISTA 
+            print('Insira uma opção válida!')
+        if option == 1:
+            print(f'{number} convertido para BINARIO é igual a {bin(number).removeprefix('0b')}')
+        if option == 2:
+            print(f'{number} convertido para OCTAL é igual a {oct(number).removeprefix('0o')}')
+        if option == 3:
+            print(f'{number} convertido para HEXADECIMAL é igual a {hex(number).removeprefix('0x')}')
+        break
+
