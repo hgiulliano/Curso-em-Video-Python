8da7fb94a8daf4f2c9df2ab67e8d1c51dab923d6
hgiulliano <henrique.giulliano@gmail.com>
2024-10-08 23:38:46 -0300

    added a counter to ex050.py

--- _Mundo2/Aula13/ex050.py
+++ _Mundo2/Aula13/ex050.py
@@ -1,8 +1,10 @@
 b = []
 soma = 0
+cont = 0
 for i in range(0,6):
     a = int(input('Digite um número inteiro: '))
     b.append(a)
     if b[i]%2 == 0: #pode substituir b[i] por a
         soma+=b[i] #pode substituir b[i] por a
-print(soma)
+        cont+=1
+print(f'Você adicionou {cont} pares e a soma deu: {soma}')
